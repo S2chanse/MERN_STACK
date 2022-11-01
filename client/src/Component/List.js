5017b8e680460e61a58557637e7d3cc0053e1507
css <seop7379@daum.net>
2022-11-01 12:14:58 +0900

    setProxy Communication

--- client/src/Component/List.js
+++ client/src/Component/List.js
@@ -1,9 +1,27 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 export default function List(props) {
+  const [text, setText] = useState('');
+  useEffect(() => {
+    let body = {
+      text: 'hello',
+    };
+    axios
+      .post('/api/test', body)
+      .then((response) => {
+        alert('연결성공');
+        let data = response.data;
+        setText(data.txt);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
   return (
     <div>
-      <h1>List</h1>
+      <h1>List {text}</h1>
       {props.contentList.map((content, idx) => {
         return (
           <div
--- server/index.js
+++ server/index.js
@@ -1,14 +1,16 @@
-const express = require("express");
-const path = require("path");
-const mongoose = require("mongoose");
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
 const app = express();
 const port = 5000;
 
-app.use(express.static(path.join(__dirname, "../client/build")));
+app.use(express.static(path.join(__dirname, '../client/build')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.listen(port, () => {
   mongoose
     .connect(
-      "mongodb+srv://indebtedness:pPkGm2kwwhCpEJOG@cluster0.l6bhxvy.mongodb.net/?retryWrites=true&w=majority"
+      'mongodb+srv://indebtedness:pPkGm2kwwhCpEJOG@cluster0.l6bhxvy.mongodb.net/?retryWrites=true&w=majority'
     )
     .then(() => {
       console.log(`Example app listening on port ${port}`);
@@ -18,11 +20,20 @@ app.listen(port, () => {
     });
 });
 
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
   //__dirname : 현재경로
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
-app.get("/express", (req, res) => {
-  res.send("Hello Express!");
+app.post('/api/test', (req, res) => {
+  console.log(req.body);
+  res.status(200).json({ success: true, txt: 'Hello' });
 });
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+/*
+  1.post MongoDB 모델
+  2.Client css(Bootstrap,Emotion)
+*/
