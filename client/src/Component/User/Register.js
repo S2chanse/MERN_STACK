69231095ec33f150d6781e7b98249a6c80691cb2
S2chanse <seop7379@daum.net>
2022-11-02 22:59:13 +0900

    set fire base register

--- client/src/Component/User/Register.js
+++ client/src/Component/User/Register.js
@@ -1,33 +1,55 @@
-import React, { useState } from 'react';
-import LoginDiv from '../../Styled/UserCSS';
+import React, { useState } from "react";
+import LoginDiv from "../../Styled/UserCSS";
+import firebase from "../../FireBase";
+import { async } from "@firebase/util";
+
 export default function () {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [pwConfirm, setPwConfirm] = useState("");
+  const RegisterFnc = async (e) => {
+    e.preventDefault();
+    if (!(name || email || password || pwConfirm)) {
+      alert("모든 값을 채워주세요.");
+      return;
+    }
+
+    if (password !== pwConfirm) {
+      alert("비밀번호가 같지 않습니다.");
+      return;
+    }
+    let createUser = await firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password);
+    await createUser.user.updateProfile({ displayName: name });
+
+    console.log(createUser.user);
+  };
   return (
     <LoginDiv>
       <form>
         <input
-          type='name'
-          placeholder='이름'
+          type="name"
+          placeholder="이름"
           onChange={(e) => setName(e.currentTarget.value)}
         />
         <input
-          type='email'
-          placeholder='이메일'
+          type="email"
+          placeholder="이메일"
           onChange={(e) => setEmail(e.currentTarget.value)}
         />
         <input
-          type='password'
-          placeholder='비밀번호'
+          type="password"
+          placeholder="비밀번호"
           onChange={(e) => setPassword(e.currentTarget.value)}
         />
         <input
-          type='password'
-          placeholder='비밀번호 확인'
-          onChange={(e) => setPassword(e.currentTarget.value)}
+          type="password"
+          placeholder="비밀번호 확인"
+          onChange={(e) => setPwConfirm(e.currentTarget.value)}
         />
-        <button>회원가입</button>
+        <button onClick={(e) => RegisterFnc(e)}>회원가입</button>
       </form>
     </LoginDiv>
   );
