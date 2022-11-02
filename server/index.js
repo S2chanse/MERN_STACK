a46ca4004a5edaa6a22dd1ee6a50e868bfeb3cec
css <seop7379@daum.net>
2022-11-02 15:47:52 +0900

    ENV(.env 방식 아님)

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const path = require('path');
 const mongoose = require('mongoose');
 const app = express();
 const port = 5000;
+const config = require('./config/key');
 
 app.use(express.static(path.join(__dirname, '../client/build')));
 app.use(express.json());
@@ -12,9 +13,7 @@ app.use('/image', express.static('./image'));
 app.use('/api/post', require('./Router/PostRouter'));
 app.listen(port, () => {
   mongoose
-    .connect(
-      'mongodb+srv://indebtedness:pPkGm2kwwhCpEJOG@cluster0.l6bhxvy.mongodb.net/?retryWrites=true&w=majority'
-    )
+    .connect(config.mongoURI)
     .then(() => {
       console.log(`Example app listening on port ${port}`);
     })
